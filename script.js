51da60b1f3af4c74e1be4d61d67c61a19fe9f9de
rutika <chughaleruti7770@gmail.com>
2025-02-27 14:31:52 +0530

    git config --global user.email "your-email@example.com"hello

---
+++ script.js
@@ -0,0 +1,7 @@
+// /**===========================typing animation===============================*/
+// var typed = new Typed(".typing",{
+//     strings:[,"Fullstack Developer"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// })
